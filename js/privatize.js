ce302350b743536fdf9fb113ba633729c536a897
Simson Garfinkel <simsong@acm.org>
2018-07-08 11:39:41 -0700

    Added comments.

--- js/privatize.js
+++ js/privatize.js
@@ -1,6 +1,17 @@
+/* ================================================================
+ * privitize.js 
+ * Work by: Simson Garfinkel (simson.l.garfinkel@census.gov)
+ *          William Yates
+ *          Dan Kifer
+ *
+ * ================================================================
+ * Public Domain
+ *
+ * ================================================================ */
 
+
+/* Return the geometric distribution for the number _p */
 function geometric(_p) {
-
    var x    = 1;
    var sum  = parseFloat(_p);
    var prod = parseFloat(_p);
@@ -16,41 +27,43 @@ function geometric(_p) {
    return x;
 }
 
+/* Return geometric noise for a specific budget and sensitivity */
+
 function geometric_noise(_budget, _sensitivity) {
    e = parseFloat(_budget) / parseFloat(_sensitivity);
-   
    p = 1.0 - Math.exp(-e);
-   
    x = geometric(p) - 1;
    y = geometric(p) - 1;
-   
    return (x - y);
-}
-
-function geometric_perturb(_ans, _budget, _sensitivity) {   
-   return (geometric_noise(_budget, _sensitivity) + _ans);
-}
-
-function laplace_perturb(_answer, _budget, _sensitivity) {
-   return _answer + laplace_noise(_budget, _sensitivity);
 }
 
 // Privacy Functions
 // From wikipedia:
 // Lap(X) = mu - b sgn(U) ln (1-2|U|) where U is a random variable between -0.5 and 0.5
+
+/* Sign function */
 function sgn(x) {
    return x < 0 ? -1 : 1;
 }
 
+/* Laplace distribution */
 function laplace(mu, b) {
    var U = Math.random() - 0.5;
    return mu - (b * sgn(U) * Math.log(1 - 2* Math.abs(U)));
 }
 
+/* Laplace noise for a specific privacy budget and sensitivity */
 function laplace_noise(_budget, _sensitivity,  ) {
    return laplace(0.0, (_sensitivity / _budget));
 }
 
-function privatize(_answer, _sensitivity, _budget) {
+/* Geometric perturbation for a specific budget and sensitivity. The results will be integers */
+function geometric_perturb(_ans, _budget, _sensitivity) {   
+   return (geometric_noise(_budget, _sensitivity) + _ans);
+}
+
+/* Laplace perturbation for a specific budget and sensitivity.  The results will be real numbers */
+function laplace_perturb(_answer, _budget, _sensitivity) {
    return _answer + laplace_noise(_budget, _sensitivity);
 }
+
